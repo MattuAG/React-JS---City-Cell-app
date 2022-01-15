88371133d6698aa8f0eaab3685c436388c9efb5d
MatuAG <matiasag1996@gmail.com>
2022-01-15 17:42:45 -0300

    se creo la carpeta cmponents y se agrego el navbar mas carrito

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,9 @@
+const CartWidget = () => {
+    return (
+        <div>
+            <img src="assets/img/carrito.png" alt="carrito" width='50'/>
+        </div>
+    )
+}
+
+export default CartWidget
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,31 @@
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import NavDropdown from "react-bootstrap/NavDropdown"
+import CartWidget from "../CartWidget/CartWidget"
+
 const NavBar = () => {
     return (
         <div>
-            Hola soy NavBar
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">City Cell</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    <Nav.Link href="#features">Inicio</Nav.Link>
+                    <Nav.Link href="#pricing">Celulares</Nav.Link>
+                    <Nav.Link href="#pricing">Accesorios</Nav.Link>
+                   
+                    </Nav>
+                    <Nav>
+                    <Nav.Link eventKey={2} href="#memes">
+                        <CartWidget/>
+                    </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+                </Navbar>
         </div>
     )
 }
