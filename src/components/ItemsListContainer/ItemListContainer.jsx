084b243b57d390558500395202256486b39e48f3
MatuAG <matiasag1996@gmail.com>
2022-01-22 23:33:28 -0300

    Contador con botón

--- src/components/ItemsListContainer/ItemListContainer.jsx
+++ src/components/ItemsListContainer/ItemListContainer.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import ItemCount from '../ItemCount/ItemCount';
+
+
+function onAdd (cant){
+  console.log(cant)
+}
 
 const ItemListContainer = (props) => {
     
-  return <div>{props.greetings}</div>;
+  return <div>
+    <button>Click</button> <br />
+    <ItemCount initial={1} stock={15} onAdd={onAdd}/>
+  </div>;
 };
 
 export default ItemListContainer;
