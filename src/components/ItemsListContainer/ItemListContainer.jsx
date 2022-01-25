74912cfba40ad947ece4ad141e1a7d2ab86d4669
MatuAG <matiasag1996@gmail.com>
2022-01-25 19:39:41 -0300

    Catálogo con MAPS y Promises

--- src/components/ItemsListContainer/ItemListContainer.jsx
+++ src/components/ItemsListContainer/ItemListContainer.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { getFetch } from '../../helpers/mock';
 import ItemCount from '../ItemCount/ItemCount';
+import ItemList from './ItemList/ItemList';
 
 
 function onAdd (cant){
   console.log(cant)
+  
 }
 
-const ItemListContainer = (props) => {
-    
-  return <div>
-    <button>Click</button> <br />
+
+function ItemListContainer () {
+  const [productos, setProductos] = useState([]);
+  const [loading, setloading] = useState(true);
+
+
+useEffect(() => {
+  getFetch
+    .then(res=> setProductos(res))
+    //.then(respuesta => console.log(respuesta))
+    .catch(err => console.log(err))
+    .finally(()=> setloading(false))
+  
+},[])
+
+
+  //console.log(productos)
+
+  return (
+    <div>
+            { loading ? 
+                    <h2> Cargando productos... </h2> 
+                : 
+                    <ItemList productos={productos} />
+            }  
+
+
+    <button /*onClick={()=> setBool (!bool)}*/>Click</button> <br />
     <ItemCount initial={1} stock={15} onAdd={onAdd}/>
-  </div>;
-};
+  </div>
+  )
+}
+
 
 export default ItemListContainer;
+
