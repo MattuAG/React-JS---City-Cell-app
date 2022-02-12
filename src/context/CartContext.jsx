ad87eccb6735e4173a28d5ab51a2b7e0fb54afeb
MatuAG <matiasag1996@gmail.com>
2022-02-12 14:38:14 -0300

    CartContext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -11,45 +11,19 @@ function CartContextProvieder({children}) {
 const [cartList, setCartList] = useState([]);
 
 function agregarAlCarrito(Item){
-    console.log(Item)
-    const index = cartList.findIndex(prod => prod.Item.id ===   Item.Item.id)
-
-    if (index === -1){
-        setCartList( [ ...cartList, Item])
-    } else {
-        const cant = cartList[index].cantidad
-        cartList[index].cantidad = Item.cantidad + cant
-        const newCartList= [...cartList]
-        setCartList(newCartList)
-    }
+    setCartList([...cartList, Item])
 }
 
-const sumaTotal =() => {
-    return cartList.reduce((acum, prod)=> acum =  acum + (prod.item.price *prod.cantidad) ,0)
-}
-
-const cantidad = () => {
-    return cartList.reduce((acum, prod) =>  acum += prod.cantidad  ,0)
-}
-
-const borrarItem = (id) => { 
-    setCartList( cartList.filter( prod => prod.item.id !== id ) )
-}
-
-function vaciarCarrito() {
+function vaciarCarrito(){
     setCartList([])
-    
+
 }
-console.log(cartList)
 
 
 return <cartContext.Provider value={{
     cartList,
     agregarAlCarrito,
     vaciarCarrito,
-    sumaTotal,
-    cantidad,
-    borrarItem,
 }} >
       {children}
 </cartContext.Provider>;
